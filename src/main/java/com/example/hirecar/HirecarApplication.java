09328bc18ffeeaca031f83048bf6d0740c49853e
纾沉 <cxy169445@alibaba-inc.com>
2019-05-18 22:22:03 +0800

    crc16

--- src/main/java/com/example/hirecar/HirecarApplication.java
+++ src/main/java/com/example/hirecar/HirecarApplication.java
@@ -7,8 +7,11 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class HirecarApplication {
+
+
     public static void main(String[] args) {
         SpringApplication.run(HirecarApplication.class, args);
+
     }
 
 }
--- src/main/java/com/example/hirecar/controller/UserController.java
+++ src/main/java/com/example/hirecar/controller/UserController.java
@@ -27,6 +27,11 @@ public class UserController {
 
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public Response login(@RequestBody LoginParam loginParam) {
+
+
+        String a = "111";
+        System.out.println(this.getCrc(a.getBytes())+ "绝望脸");
+        System.out.println(this.getCrc("22222222".getBytes())+ "绝望脸");
         UserCachePDO userCachePDO = userService.sureLogin(loginParam);
 
         if(userCachePDO == null) {
@@ -54,6 +59,31 @@ public class UserController {
         System.out.println(list);
         return new Response(list);
 
+    }
+
+    private String getCrc(byte[] data) {
+        int high;
+        int flag;
+
+        // 16位寄存器，所有数位均为1
+        int wcrc = 0xffff;
+        for (int i = 0; i < data.length; i++) {
+            // 16 位寄存器的高位字节
+            high = wcrc >> 8;
+            // 取被校验串的一个字节与 16 位寄存器的高位字节进行“异或”运算
+            wcrc = high ^ data[i];
+
+            for (int j = 0; j < 8; j++) {
+                flag = wcrc & 0x0001;
+                // 把这个 16 寄存器向右移一位
+                wcrc = wcrc >> 1;
+                // 若向右(标记位)移出的数位是 1,则生成多项式 1010 0000 0000 0001 和这个寄存器进行“异或”运算
+                if (flag == 1)
+                    wcrc ^= 0xa001;
+            }
+        }
+
+        return Integer.toHexString(wcrc);
     }
 
 
